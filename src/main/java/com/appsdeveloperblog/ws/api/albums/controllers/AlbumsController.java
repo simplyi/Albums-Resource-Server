6f8cea571b8393d5dd35c6a5eeab37843bb27749
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-09-14 10:31:29 -0400

    Updated titles of both albums

--- src/main/java/com/appsdeveloperblog/ws/api/albums/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/albums/controllers/AlbumsController.java
@@ -22,14 +22,14 @@ public class AlbumsController {
         AlbumRest album1 = new AlbumRest();
         album1.setAlbumId("albumIdHere");
         album1.setUserId("1");
-        album1.setAlbumTitle("Album 1 title");
+        album1.setAlbumTitle("Album one title");
         album1.setAlbumDescription("Album 1 description");
         album1.setAlbumUrl("Album 1 URL");
         
         AlbumRest album2 = new AlbumRest();
         album2.setAlbumId("albumIdHere");
         album2.setUserId("2");
-        album2.setAlbumTitle("Album 2 title");
+        album2.setAlbumTitle("Album two title");
         album2.setAlbumDescription("Album 2 description");
         album2.setAlbumUrl("Album 2 URL");
          
