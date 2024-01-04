22a91c04df55c003caab023d5190f75b90d7b41e
simplyi <sergey.kargopolov@gmail.com>
2024-01-04 11:07:26 -0500

    Spring Boot 3.2.1 update

--- src/main/java/com/appsdeveloperblog/ws/api/albums/security/WebSecurityConfig.java
+++ src/main/java/com/appsdeveloperblog/ws/api/albums/security/WebSecurityConfig.java
@@ -5,35 +5,37 @@
  */
 package com.appsdeveloperblog.ws.api.albums.security;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter()); // delegate to custom converter
-
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/photos/**")
-                .hasAuthority("ROLE_developer")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthenticationConverter); // apply jwt converter;
-
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
+		http.authorizeHttpRequests(
+				authz -> authz.requestMatchers(new AntPathRequestMatcher("/albums/**", "GET")).hasAuthority("ROLE_developer")
+				.anyRequest().authenticated())
+		.oauth2ResourceServer(
+						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+		
+		http.sessionManagement((session) -> session
+		        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		return http.build();
     }
 
     
